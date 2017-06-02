b76840c60a9c6fadf3fe32651e5892872ae949d0
Armen <armen.arzumanyan@gmail.com>
2017-06-02 14:04:05 +0000

    Add new file

---
+++ java_shell.sh
@@ -0,0 +1,12 @@
+String varHello = "Hello Java 9 SE!";
+
+System.out.println(varHello);
+
+
+public class MyClazz{
+
+    public void sayHello(){
+        System.out.println("Strange HelloWorld behavior in Java 9 :)");
+    }
+}
+new MyClazz().sayHello()
